fab3df1fdbd1f72e9c38a70a9184cf4c5434c83f
alanmark13579 <korosu1128@gmail.com>
2021-01-04 21:33:48 +0800

    fixed

--- CarShop/src/carshop/Car.java
+++ CarShop/src/carshop/Car.java
@@ -2,9 +2,9 @@ package carshop;
 
 abstract public class Car implements Navigation,Air_conditioner { //方向盤 輪子
 
-        abstract public String getName();   //汽車名稱
-        abstract public int getPrice() ;  //獲得汽車售價
-        abstract public String drive_position();
+    abstract public String getName();   //汽車名稱
+    abstract public int getPrice() ;  //獲得汽車售價
+    abstract public String drive_position();
 }
 
 interface Navigation {
@@ -14,36 +14,42 @@ interface Air_conditioner {
     String Air_conditioner_name() ;
 }
 class BMW extends Car  {
+    int price = 300000;
 
+    @Override
     public String Navigation_name(){
         return "NAV_TW" ;
     }
+    @Override
     public String Air_conditioner_name(){
         return "AIR_TW" ;
     }
+    @Override
     public String getName() {
         return "BMW";
     }
-    public int getPrice() {
-        return 300000;
-    }
-    public String drive_position(){
-        return "Right" ;
-    }
+    @Override
+    public int getPrice() { return price; }
+    @Override
+    public String drive_position(){ return "Right" ; }
 }
 class TOYOTA extends Car {
+    int price =200000;
+    @Override
     public String Navigation_name(){
         return "NAV_JP" ;
     }
-    public String Air_conditioner_name(){
-        return "AIR_JP" ;
-    }
+    @Override
+    public String Air_conditioner_name(){ return "AIR_JP" ; }
+    @Override
     public String getName() {
         return "TOYOTA";
     }
+    @Override
     public int getPrice() {
-        return 200000;
+        return price;
     }
+    @Override
     public String drive_position(){
         return "Left" ;
     }
